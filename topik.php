02eae0b1599d6ec753b722a0f1895d36d037131c
Wildan M <willnode@wellosoft.net>
2020-12-13 17:36:12 +0700

    Koreksi CSS + HTML

--- topik.php
+++ topik.php
@@ -1,9 +1,9 @@
 <?php
-#require 'include/adminPermission.inc'; 
+require 'include/adminPermission.inc';
 include "include/koneksi.inc";
 $topic=$_GET['topik'];
 $query = $dbc->prepare("SELECT * FROM topic WHERE topic_id = :topic");
-        $query->bindvalue(":topic", $topic);
-        $query->execute();
-        $topic=$query->fetch();
+$query->bindvalue(":topic", $topic);
+$query->execute();
+$topic=$query->fetch();
 include "view/topik.inc";
