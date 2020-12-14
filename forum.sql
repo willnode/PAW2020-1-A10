bc00cf30c610fe873257e84cac1f27cb378484e3
Rizal0063 <48111980+Rizal0063@users.noreply.github.com>
2020-12-14 13:44:51 +0700

    validate clear + update db

--- forum.sql
+++ forum.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2020 at 11:19 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Waktu pembuatan: 14 Des 2020 pada 06.46
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,48 +20,59 @@ SET time_zone = "+00:00";
 --
 -- Database: `forum`
 --
+CREATE DATABASE IF NOT EXISTS `forum` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `forum`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `answer`
+-- Struktur dari tabel `answer`
+--
+-- Pembuatan: 14 Des 2020 pada 05.29
 --
 
+DROP TABLE IF EXISTS `answer`;
 CREATE TABLE `answer` (
   `answer_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `thread_id` int(11) NOT NULL,
-  `answer` varchar(1024) COLLATE utf8_bin NOT NULL,
+  `answer` varchar(8000) COLLATE utf8_bin NOT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `thread`
+-- Struktur dari tabel `thread`
+--
+-- Pembuatan: 14 Des 2020 pada 05.28
 --
 
+DROP TABLE IF EXISTS `thread`;
 CREATE TABLE `thread` (
   `thread_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `topic_id` int(11) NOT NULL,
-  `question` varchar(1024) COLLATE utf8_bin NOT NULL,
+  `question` varchar(8000) COLLATE utf8_bin NOT NULL,
   `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `topic`
+-- Struktur dari tabel `topic`
+--
+-- Pembuatan: 14 Des 2020 pada 05.25
 --
 
+DROP TABLE IF EXISTS `topic`;
 CREATE TABLE `topic` (
   `topic_id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `topic`
+-- Dumping data untuk tabel `topic`
 --
 
 INSERT INTO `topic` (`topic_id`, `title`) VALUES
@@ -75,9 +86,13 @@ INSERT INTO `topic` (`topic_id`, `title`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
+--
+-- Pembuatan: 14 Des 2020 pada 05.25
+-- Pembaruan terakhir: 14 Des 2020 pada 05.28
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -89,18 +104,18 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`user_id`, `email`, `password`, `nama`, `bio`, `avatar`, `type`) VALUES
-(1, 'admin@gmail.com', 'fcf730b6d95236ecd3c9fc2d92d7b6b2bb061514961aec041d6c7a7192f592e4', 'admin', 'test', 'pic.jpg', 'expert');
+(1, 'expert@gmail.com', 'fcf730b6d95236ecd3c9fc2d92d7b6b2bb061514961aec041d6c7a7192f592e4', 'Expert', 'test', 'profile.png', 'expert');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `answer`
+-- Indeks untuk tabel `answer`
 --
 ALTER TABLE `answer`
   ADD PRIMARY KEY (`answer_id`),
@@ -108,7 +123,7 @@ ALTER TABLE `answer`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `thread`
+-- Indeks untuk tabel `thread`
 --
 ALTER TABLE `thread`
   ADD PRIMARY KEY (`thread_id`),
@@ -116,58 +131,58 @@ ALTER TABLE `thread`
   ADD KEY `user` (`user_id`);
 
 --
--- Indexes for table `topic`
+-- Indeks untuk tabel `topic`
 --
 ALTER TABLE `topic`
   ADD PRIMARY KEY (`topic_id`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `answer`
+-- AUTO_INCREMENT untuk tabel `answer`
 --
 ALTER TABLE `answer`
   MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `thread`
+-- AUTO_INCREMENT untuk tabel `thread`
 --
 ALTER TABLE `thread`
   MODIFY `thread_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `topic`
+-- AUTO_INCREMENT untuk tabel `topic`
 --
 ALTER TABLE `topic`
   MODIFY `topic_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `answer`
+-- Ketidakleluasaan untuk tabel `answer`
 --
 ALTER TABLE `answer`
   ADD CONSTRAINT `thread` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`thread_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `thread`
+-- Ketidakleluasaan untuk tabel `thread`
 --
 ALTER TABLE `thread`
   ADD CONSTRAINT `topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE,
